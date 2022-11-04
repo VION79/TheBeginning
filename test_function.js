2aca2a54ffc8f7005828c0d1a0bf53d364631b7f
VION79 <vion79@gmail.com>
2022-11-04 12:57:28 -0400

    working with js operators

---
+++ test_function.js
@@ -0,0 +1,14 @@
+function addSomething (num, str) {
+
+    //all statements go inside curly braces
+
+    var add = num + str;
+
+    alert (add); 
+}
+
+// function needs to be called outside of the function to work
+
+addSomething (455," programs to write");
+
+// javascript is a way to tie a function to an event such as a mouse click
---
+++ test_operators.js
@@ -0,0 +1,14 @@
+//Arithmetic and Assignment Operators
+
+var number1 = 7;
+var number2 = 4;
+
+// var number1 = number1 + number2;
+
+number1 += number2;
+number1 ++;
+number1 --;
+
+document.write (number1);
+
+

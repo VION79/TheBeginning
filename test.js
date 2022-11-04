d5122005eb028be368a7bd1327d2fa52a90b6489
VION79 <vion79@gmail.com>
2022-11-04 11:39:40 -0400

    using exteral js file

---
+++ test.js
@@ -0,0 +1,12 @@
+document.write ("We are now using Javascript file");
+                                            //var
+                                            //Must always start with VAR
+                var day;                    //Declare variable without assigning value
+                var assignValue = 23454;    //Declare variable and assign a value
+                var years = 35;             //Numeric variables - numbers
+                var someText = " years old";//String variables - text and characters - MUST use double quatation marks
+                var gameOver = true;        //Boolean - True or False
+                                            //Function is the action or behaviour. Where we wokr with our variables
+                                            //Variable naming: can only start with letter or _
+                                            //Variables are case sensitive
+                alert (years + someText);

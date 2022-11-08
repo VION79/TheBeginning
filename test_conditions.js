5d0596f20d5b7e63d288eb576a18fc82b336938f
VION79 <vion79@gmail.com>
2022-11-07 19:04:49 -0500

    conditionals

---
+++ test_conditions.js
@@ -0,0 +1,22 @@
+/* var food = "Apple"; */
+
+var food = "meat";
+
+if (food == "Apple" ) {
+
+alert ("We ate some fruit and specifically an Apple");
+
+}
+
+else if (food == "meat") {
+    
+    alert ("We ate some meat")
+}
+
+/* else statement is always followed by the if statement but if statement could stand alone */
+else {
+alert ("We ate something else")    
+}
+
+/* We need to make sure that the file is within the right folder. If the file is not in the website
+folder you are woking on it will not launch the .js that we are calling.*/

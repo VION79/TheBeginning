e43231f63c2c18c53ea51fcbf3d8c032818300af
VION79 <vion79@gmail.com>
2022-11-09 14:55:27 -0500

    buttons

--- test.js
+++ test.js
@@ -13,14 +13,19 @@ document.write ("We are now using Javascript file");
                 alert (years + someText);
 */
 //global and local variables
-var name="Henry";                           //global variable
+var name="Henry is a global variable";                           //global variable
+
 function printName() {
-var name2="Sarah";                      /*local variable; when we omit var in front of name2, it will treat
-                                        it like global variable. If I want the var to be treated like a local
-                                        I must put var in front of the variable*/
+var name2="Sarah is a local Variable";                          //local variable 
+/*local variable; when we omit var in front of name2, it will treat it like global variable. 
+If I want the var to be treated like a local I must put var in front of the variable. Otherwise it would be
+treated like a global var*/
+
+document.write(name);                   
+
+/* If I want to document.write local var name2, I must place the code inside the function.*/
 
 }
 
 printName();                                //call function
-document.write(name2);                   /* If I want to document.write name to, I must place the code inside
-                                            the function.*/
+
